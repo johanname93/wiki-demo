89726b07285c439e0e7faddf8cce3a09b37ab169
Johanna <johanna.messerschmidt@gmail.com>
2022-11-01 11:54:57 +0100

    add basic html template in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IArticle } from './interfaces/article.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'wiki';
+
+  article: IArticle = {
+    title: 'Digitale Gesundheitsanwendung',
+    definition: `Als <span class="bold">Digitale Gesundheitsanwendungen</span> (abgekürzt 
+      <span class="italic">DiGA</span>) werden
+      <a href="https://de.wikipedia.org/wiki/Medizinprodukt">Medizinprodukte</a>
+      mit gesundheitsbezogener Zweckbestimmung bezeichnet, deren Hauptfunktion
+      wesentlich auf
+      <a
+        href="https://de.wikipedia.org/wiki/Informations-_und_Kommunikationstechnik"
+        >digitalen Technologien</a
+      >
+      beruht (z. B. „Gesundheits-Apps“) und die von den Krankenkassen erstattet
+      werden. Sie sind dazu bestimmt, die Förderung der Gesundheit sowie die
+      Erkennung, Überwachung, Behandlung von Krankheiten und Behinderung zu
+      unterstützen.[1]`
+
+  }
+
+  
 }
