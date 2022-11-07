e2c577348e277d36d4c6b49187f663804e5fcacb
Johanna <johanna.messerschmidt@gmail.com>
2022-11-07 16:43:42 +0100

    add sections and subsections

--- src/app/interfaces/article.interface.ts
+++ src/app/interfaces/article.interface.ts
@@ -1,4 +1,13 @@
 export interface IArticle{
     title: string;
     definition: string;
+  
+    sections: {
+        title: string;
+        content: string;
+        subsections: {
+            title: string;
+            content: string;
+        }[];
+    }[];
 }
